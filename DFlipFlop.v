c327c27eb5b57a7ecf406bc5240a6024dfac2f3f
Aaron <57469001+ohlookitsaaron@users.noreply.github.com>
2019-11-20 18:22:06 -0800

    updated from 8bitworkshop.com

--- DFlipFlop.v
+++ DFlipFlop.v
@@ -1,12 +1,10 @@
 //time measurement unit 1 nsec with 100 ps precision
 `timescale 1ns/100ps
 
-// Design of Positive Edge D-Flip-Flop
-
 // input/output of module
 module DFlipFlop(
-  input wire clk, d, // input
-  output reg q	// output
+  input wire clk, d,
+  output reg q
 );
 
   always @(posedge clk) // executes at every clock rising edge
