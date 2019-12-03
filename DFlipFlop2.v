28aeb5bd6b975bc5d132c6a0568b6b7804445692
Aaron <57469001+ohlookitsaaron@users.noreply.github.com>
2019-12-02 17:17:53 -0800

    updated from 8bitworkshop.com

---
+++ DFlipFlop2.v
@@ -0,0 +1,16 @@
+//time measurement unit 1 nsec with 100 ps precision
+`timescale 1ns/100ps
+
+// input/output of module
+module DFlipFlop2(
+  input wire clk, s, r,
+  output reg q, reg qi
+);
+
+  always @(posedge clk) // executes at every clock rising edge
+    begin
+      q <= s;
+      qi <= r;
+    end
+  
+endmodule
